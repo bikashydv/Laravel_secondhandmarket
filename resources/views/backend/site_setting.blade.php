d1ff1effe5b85b075dac808d487176b8e41042fa
Bikash <techbmb10@gmail.com>
2022-03-03 08:10:57 +0545

    side setting

--- resources/views/backend/site_setting.blade.php
+++ resources/views/backend/site_setting.blade.php
@@ -1,5 +1,42 @@
 @extends('backend.layouts.master')
 @section('content')
 
-Hello
+    <form>
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlFile1">Logo</label>
+                <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+        </form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">System name</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="System name">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Mobile number</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="mobile number">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Phone number</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="phone number">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Adress</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="Adress">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Social media</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="facbook, instagram,">
+        </div>
+
+    </form>
 @endsection
