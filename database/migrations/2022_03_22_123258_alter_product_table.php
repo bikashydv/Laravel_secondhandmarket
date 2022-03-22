cf40795860b6a37f87c4cc633be1518c14f08630
Bikash <techbmb10@gmail.com>
2022-03-22 19:17:40 +0545

    product work

---
+++ database/migrations/2022_03_22_123258_alter_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('author')->nullable();
+            $table->string('publish_on')->nullable();
+            $table->string('image')->nullable();
+            $table->string('action')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
