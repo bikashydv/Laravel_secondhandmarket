e289cf92dbb4e02ec64e2c1266b5435048f84e5f
Bikash <techbmb10@gmail.com>
2022-03-31 06:36:59 +0545

    recent add

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -171,7 +171,6 @@
         <img src="./images/1.jpg " alt="banner-image" id="mainBannerImg">
     </div>
     <!-- /banner -->
-
     <!-- popular categories -->
     @include('frontend.includes.categoriesdown')
     <!-- /popular categories -->
@@ -185,6 +184,12 @@
                 <hr>
             </div>
             <div class="picks" id="picks">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
 
             </div>
         </div>
